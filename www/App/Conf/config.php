52712807e26070de851c179f12cfa42776e6779d
Bin Zhang <zhangbinsjtu@qq.com>
2014-01-11 20:56:45 +0800

    just for checkout the branch to wanglei

--- www/App/Conf/config.php
+++ www/App/Conf/config.php
@@ -3,7 +3,7 @@ return array(
 	//'配置项'=>'配置值'
 	//开启应用分组
 	'APP_GROUP_LIST' => 'Home,Admin',
-	'DEFAULT_GROUP' => 'Home',		//默认分组
+	'DEFAULT_GROUP' => 'Home',  //默认分组
 
 	// 添加数据库配置信息
 	'DB_TYPE'   => 'mysql',   // 数据库类型
@@ -14,7 +14,10 @@ return array(
 	'DB_NAME'   => 'dwms',  // 数据库名
 	'DB_PREFIX' => '',  // 数据库表前缀
 
-	'SHOW_PAGE_TRACE' =>true, // 显示页面Trace信息
+	// 显示页面Trace信息
+	'SHOW_PAGE_TRACE' =>true,
+	// 关闭布局模板配置，使用layout控制模板布局
+	'LAYOUT_ON'=>false,
 	//点语法默认解析
 	//'TMPL_VAR_IDENTIFY' => 'array',
 	//模板路径，重定义控制器名称和方法模板名称的分隔符
