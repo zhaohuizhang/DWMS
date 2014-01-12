c7cfa59994511d068c4bc8939b4f21bb3fbf9cc8
Wang Lei <nihiue@gmail.com>
2014-01-12 13:20:34 +0800

    t

--- www/App/Lib/Action/Home/LoginAction.class.php
+++ www/App/Lib/Action/Home/LoginAction.class.php
@@ -14,7 +14,7 @@ class LoginAction extends CommonAction{
 	public function country() {
 		if ( session( 'user_type' ) == 1 ) {
 			layout('./Common/frame');
-			$this->display('./Content/Government/country_index');
+			$this->display('./Content/Government/homepage/country_index');
 
 			//$this->display();
 		}else {
