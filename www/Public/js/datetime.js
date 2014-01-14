c7229f8f96ac1360b3c01c5de72d231c4551e924
Wang Lei <nihiue@gmail.com>
2014-01-14 23:54:47 +0800

    logo ^4

--- www/Public/js/datetime.js
+++ www/Public/js/datetime.js
@@ -3,7 +3,7 @@
 function showDateTime() {
     var DateObj = new Date();
     var DateTime = DateObj.toLocaleString();
-    $("#datetime").html('<span class="glyphicon glyphicon-user"></span> 当前时间：' + DateTime);
+    $("#datetime").html('<span class="glyphicon glyphicon-time"></span> 当前时间：' + DateTime);
 }
 showDateTime();
 setInterval("showDateTime()", 1000);
