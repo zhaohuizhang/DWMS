0fae313d332c3370deebf747efe06e9fa60a5641
zhaohui <zhangzhaohui@sjtu.edu.cn>
2014-01-18 00:04:59 +0800

    add android api

---
+++ www/App/Lib/Action/Home/AndroidApiAction.class.php
@@ -0,0 +1,36 @@
+﻿<?php
+/**
+ *
+ */
+class AndroidApiAction extends Action{
+	
+	// 获取废物的名称
+	public function getWasteName($imei=0){
+		
+	}
+	
+	// 增加废物重量
+	public function addWaste(){
+	
+	}
+	
+	// 绑定RFID
+	public function bindRfid(){
+	
+	}
+	
+	// 获取RFID绑定的废物信息
+	public function getRfidWasteName(){
+	
+	}
+	
+	//废物入库
+	public function wasteIn(){
+	
+	}
+	
+	//废物出库
+	public function wasteOut(){
+	
+	}
+?>
