3ede05478346e56e5a01fcc1f5940ee20e540d3a
Bin Zhang <zhangbinsjtu@qq.com>
2014-01-17 14:05:56 +0800

    change the IP of database

--- www/App/Conf/config.php
+++ www/App/Conf/config.php
@@ -8,7 +8,7 @@ return array(
 	// 添加数据库配置信息
 	'DB_TYPE'   => 'mysql',   // 数据库类型
 	// 'DB_HOST'   => '10.50.6.70', // 服务器地址
-	'DB_HOST'	=> 'localhost',	// 本地服务器地址
+	'DB_HOST'	=> '202.120.58.120',	// 本地服务器地址
 	'DB_USER'   => 'root',    // 用户名
 	'DB_PWD'    => 'root1234',  // 密码
 	'DB_PORT'   => 3306,    // 端口
