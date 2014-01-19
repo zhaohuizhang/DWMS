9812973b46ab250464758a64904fade0d9375538
Bin Zhang <zhangbinsjtu@qq.com>
2014-01-19 20:38:40 +0800

    complete the production's record

--- www/App/Lib/Action/Home/LoginCityAction.class.php
+++ www/App/Lib/Action/Home/LoginCityAction.class.php
@@ -140,7 +140,7 @@ class LoginCityAction extends CommonAction{
 				break;
 				// 业务办理->待办业务->转移备案管理
 			case 'transfer_record_management':
-				$record = M( 'record' )->getField( 'record_id,record_code,record_date,record_status' );
+				$record = M( 'record' )->where('record_status>0')->getField( 'record_id,record_code,record_date,record_status' );
 				$record_json = json_encode( $record );
 
 				$unit_name = M( 'production_unit' )->getField( 'production_unit_name' );
@@ -170,7 +170,12 @@ class LoginCityAction extends CommonAction{
 					'record_id' => $record_id,
 					'record_status' => $record_status,
 				);
-				M( 'record' )->save( $current_record_status );
+				$result = M( 'record' )->save( $current_record_status );
+				if ($result) {
+					$this->ajaxReturn(1, '审核成功！', 1);
+				} else {
+					$this->ajaxReturn(0, '审核失败！', 0);
+				}
 				break;
 				// 业务办理->待办业务->转移联单管理
 			case 'transfer_manifest_management':
@@ -186,31 +191,31 @@ class LoginCityAction extends CommonAction{
 				$this->ajaxReturn( "$tmp_content");
 				break;
 			case 'enterprise_user_management_page_production':
-				$production_unit = M( 'production_unit' )->where(array('user_id' => $record_id ))->find();				
+				$production_unit = M( 'production_unit' )->where(array('user_id' => $record_id ))->find();
 				$this->formData = $production_unit;
 				$tmp_content=$this->fetch( './Public/html/Content/City/business/enterprise_user_management_page_production.html' );
 				$this->ajaxReturn( $tmp_content );
 				break;
 			case 'enterprise_user_management_page_transport':
-				$transport_unit = M( 'transport_unit' )->where(array('user_id' => $record_id ))->find();				
+				$transport_unit = M( 'transport_unit' )->where(array('user_id' => $record_id ))->find();
 				$this->formData = $transport_unit;
 				$tmp_content=$this->fetch( './Public/html/Content/City/business/enterprise_user_management_page_transport.html' );
 				$this->ajaxReturn( $tmp_content );
 				break;
 			case 'enterprise_user_management_page_reception':
-				$reception_unit = M( 'reception_unit' )->where(array('user_id' => $record_id ))->find();				
+				$reception_unit = M( 'reception_unit' )->where(array('user_id' => $record_id ))->find();
 				$this->formData = $reception_unit;
 				$tmp_content=$this->fetch( './Public/html/Content/City/business/enterprise_user_management_page_reception.html' );
 
 				$this->ajaxReturn( $tmp_content );
 				break;
 			case 'enterprise_user_management_ajaxpost':
-			
-				$munit=M( 'user' );					
-				
+
+				$munit=M( 'user' );
+
 				//$this->show(json_encode($line));
 				if(I( 'post.action' )=="lock")
-				{	
+				{
 					if(I( 'post.value' )=='0')
 						$data['lock'] = '0';
 					else
@@ -224,18 +229,18 @@ class LoginCityAction extends CommonAction{
 						$data['is_verify'] = '0';
 					else
 						$data['is_verify'] = '1';
-					
+
 					$munit->where(array('user_id' =>I('post.user_id')))->save($data);
 					$this->show("verify_ok".I('post.user_id'));
-				}	
-				
+				}
+
 				else
 				{
 					$this->error("action_error");
 				}
-		
+
 				break;
-			
+
 				// 业务办理->待办业务->企业信息管理
 			case 'enterprise_information_management':
 
