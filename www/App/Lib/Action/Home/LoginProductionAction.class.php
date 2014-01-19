40edc15eda4f53140e124560b745fd3973fd618b
Bin Zhang <zhangbinsjtu@qq.com>
2014-01-19 18:47:13 +0800

    almost complete the production's record

--- www/App/Lib/Action/Home/LoginProductionAction.class.php
+++ www/App/Lib/Action/Home/LoginProductionAction.class.php
@@ -6,11 +6,13 @@ class LoginProductionAction extends CommonAction{
 	public function production( $id="production_index", $record_id="" ) {
 		if ( session( 'user_type' ) == 5 ) {
 			switch ( $id ) {
+
 				// -------- 生产企业->首页 --------
 			case 'production_index':
 				layout( './Common/frame' );
 				$this->display( './Public/html/Content/Production/homepage/production_index.html' );
 				break;
+
 
 				// -------- 企业基本信息->侧边栏 --------
 			case 'basic_sidebar':
@@ -32,6 +34,7 @@ class LoginProductionAction extends CommonAction{
 				$this->ajaxReturn( $tmp_content );
 				break;
 
+
 				// -------- 危废库存->侧边栏 --------
 			case 'warehouse_sidebar':
 				layout( './Common/frame' );
@@ -52,6 +55,7 @@ class LoginProductionAction extends CommonAction{
 				$tmp_content=$this->fetch( './Public/html/Content/Production/warehouse/storage_output_management.html' );
 				$this->ajaxReturn( $tmp_content );
 				break;
+
 
 				// -------- 转移备案->侧边栏 --------
 			case 'record_sidebar':
@@ -84,7 +88,6 @@ class LoginProductionAction extends CommonAction{
 				} else {
 					$this->ajaxReturn(0, '保存失败！', 0);
 				}
-
 				break;
 				// 转移备案->转移备案查询
 			case 'transfer_record_query':
@@ -95,17 +98,98 @@ class LoginProductionAction extends CommonAction{
 				$unit_json = json_encode( $unit_name );
 
 				$tmp_content=$this->fetch( './Public/html/Content/Production/record/transfer_record_query.html' );
-				$tmp_content = "<script>record_json = $record_json; unit_json = $unit_json</script> $tmp_content";
+				$tmp_content = "<script>record_json = $record_json; unit_json = $unit_json; </script> $tmp_content";
 				$this->ajaxReturn( $tmp_content );
 				break;
 				// 转移备案->转移备案查询->详细信息页
-			case 'transfer_record_query_page':
+			case 'transfer_record_query_detail':
 				$record = M( 'record' )->where( array('record_id' =>$record_id))->find();
-				$production_unit = M( 'production_unit' )->where( array('production_unit_id' => $record['production_unit_id']))->find();
+				$production_unit = M( 'production_unit' )->where( array('production_unit_id' => session('production_unit_id')))->find();
 				$this->record = $record;
 				$this->unit = $production_unit;
-				$tmp_content=$this->fetch( './Public/html/Content/Production/record/transfer_record_query_page.html' );
+
+				$tmp_content=$this->fetch( './Public/html/Content/Production/record/transfer_record_query_detail.html' );
 				$this->ajaxReturn( $tmp_content );
+				break;
+				// 转移备案->转移备案查询->修改信息页
+			case 'transfer_record_query_modify':
+				$record = M( 'record' )->where( array('record_id' =>$record_id))->find();
+				$production_unit = M( 'production_unit' )->where( array('production_unit_id' => session('production_unit_id')))->find();
+				$this->record = $record;
+				$this->unit = $production_unit;
+
+				$record_id_json = json_encode( $record_id );
+				$record_status_json = json_encode( $record['record_status'] );
+
+				$tmp_content=$this->fetch( './Public/html/Content/Production/record/transfer_record_query_modify.html' );
+				$tmp_content = "<script>record_id_json = $record_id_json; record_status_json = $record_status_json; </script> $tmp_content";
+				$this->ajaxReturn( $tmp_content );
+				break;
+				// 转移备案->转移备案查询->修改提交
+			case 'transfer_record_query_modified':
+				$record = M( 'record' ); // 实例化record对象
+				$record->create(); // 根据表单提交的POST数据创建数据对象
+				$record->record_id = $record_id;
+				$time = date( 'Y-m-d H:i:s', time() );
+				$record->record_modify_time = $time;
+				$record_status_old = I('post.record_status_old');
+				switch ($record_status_old){
+					case '0':
+						$record_status = 0;
+						break;
+					case '3':
+						$record_status = 4;
+						break;
+					case '4':
+						$record_status = 4;
+						break;
+					default:
+						$record_status = -1;
+						break;
+				}
+				$record->record_status = $record_status;
+				$result = $record->save(); // 根据条件保存修改的数据
+
+				if ($result) {
+					$this->ajaxReturn(1, '修改成功！', 1);
+				} else {
+					$this->ajaxReturn(0, '修改失败！', 0);
+				}
+				break;
+				// 转移备案->转移备案查询->修改提交
+			case 'transfer_record_query_submit':
+				$record = M( 'record' )->where( array('record_id' =>$record_id))->find();
+				$production_unit = M( 'production_unit' )->where( array('production_unit_id' => session('production_unit_id')))->find();
+				$this->record = $record;
+				$this->unit = $production_unit;
+
+				$record_id_json = json_encode( $record_id );
+				$record_status_json = json_encode( $record['record_status'] );
+
+				$tmp_content=$this->fetch( './Public/html/Content/Production/record/transfer_record_query_submit.html' );
+				$tmp_content = "<script>record_id_json = $record_id_json; record_status_json = $record_status_json; </script> $tmp_content";
+				$this->ajaxReturn( $tmp_content );
+				break;
+
+			case 'transfer_record_query_submited':
+				$record_status_old = I('post.record_status_old');
+				switch ($record_status_old){
+					case '0':
+						$record_status = 1;
+						break;
+					case '4':
+						$record_status = 1;
+						break;
+					default:
+						$record_status = -1;
+						break;
+				}
+				$record = M( 'record' ); // 实例化record对象
+				$data['record_id'] = $record_id;
+				$data['record_status'] = $record_status;
+				$record->save($data);
+				break;
+
 
 				// -------- 转移联单->侧边栏 --------
 			case 'manifest_sidebar':
@@ -122,6 +206,7 @@ class LoginProductionAction extends CommonAction{
 				$tmp_content=$this->fetch( './Public/html/Content/Production/manifest/transfer_manifest_query.html' );
 				$this->ajaxReturn( $tmp_content );
 				break;
+
 
 			default:
 				$this->error( "页面不存在！" );
