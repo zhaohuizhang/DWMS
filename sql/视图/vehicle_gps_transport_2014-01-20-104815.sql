400819ea89710096419c872babfa63ed15bf9914
Bin Zhang <zhangbinsjtu@qq.com>
2014-01-30 09:28:47 +0800

    delete some early sql files

--- sql/视图/vehicle_gps_transport_2014-01-20-104815.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `vehicle_gps_transport` (`transport_unit_name`, `vehicle_type`, `device_serial_num`, `vehicle_status`) VALUES ('xxx运输单位', '大卡车', '308001061692', 0);
-INSERT INTO `vehicle_gps_transport` (`transport_unit_name`, `vehicle_type`, `device_serial_num`, `vehicle_status`) VALUES ('this this for test', '货车', '308033501795', 1);
